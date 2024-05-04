4ac94dd864eed467a7fd1bd4c0bb18c9e2faf826
navidmirzad <navi0221@stud.kea.dk>
2024-05-04 16:42:30 +0200

    dialogueTree

--- script.js
+++ script.js
@@ -2,61 +2,88 @@ window.addEventListener("load", start);
 
 let currentScene;
 
-const scene1A = {
-  title: "At the Lake",
-  text: /*html*/ `
-        <p>WUHUU NICE weather</p>
-        <p>Im getting kind of hungry...</p>
-        <p>I should get something to eat</p>`,
-  choices: [
-    { name: "Pizza", node: null },
-    { name: "Go home and cook dinner", node: null },
-  ],
-};
-
-const scene1B = {
-  title: "On my way to get a Pizza",
-  text: /*html*/ `
-        <p>FeelsGoodMan</p>
-        <p>Let me order the kebab pizza yum...</p>
-        <p>THEY DONT HAVE KEBAB?!</p>`,
-  choices: [
-    { name: "Find another place to eat Pizza", node: null },
-    { name: "Eat a different Pizza today?", node: null },
-    { name: "Go home and cook dinner", node: null },
-  ],
-};
-
-const scene1C = {
-  title: "Home and cooking dinner",
-  text: /*html*/ `
-        <p>Im so tired I should have gotten the pizza</p>
-        <p></p>
-        <p>Im out of milk NOO</p>`,
-  choices: [
-    { name: "Order a pizza", node: null },
-    { name: "Starve yourself", node: null },
-    { name: "Go grocery shopping", node: null },
-  ],
-};
-
 // Story begins here:
-const scene1 = {
+const startScene = {
   title: /*html*/ "Story begins",
   text: `
             <p>It's a warm summer day and you're planning on going to the Lake to have a calm day off.</p>
             <p>Question is how do we get to the lake? All public transportation is not in service this week...</p>
             <p>What are we gonna do?</p>`,
   choices: [
-    { name: "Choice #1", node: scene1A },
-    { name: "Choice #2", node: scene1B },
+    { name: "Take the bike", node: null },
+    { name: "Stay home and play video games", node: null },
   ],
+};
+
+const lakeScene = {
+  title: "At the Lake",
+  text: /*html*/ `
+        <p>Finally, we're finally at the lake... Let's go and lay down by the water and just sunbathe for a bit.</p>
+        <p>I got my sunglasses, my sunscreen and my speaker for some great vibes... Life sure is sweet!</p>
+        <p>I FORGOT MY SNACKS, HOW COULD THIS HAPPEN. *stomach says brurururu*... I should get something to eat</p>`,
+  choices: [
+    { name: "Go get a pizza", node: null },
+    { name: "Go home and cook dinner", node: null },
+  ],
+};
+
+const homeScene = {
+  title: "Video games at home",
+  text: /*html*/ `
+        <p>I'm, actually completely okay with staying home lol.</p>
+        <p>TIME TO PLAY THAT NEW GAME I GOT LAST WEEK, WOOO!</p>
+        <p>I'm getting kind of hungry though... I should get something to eat</p>`,
+  choices: [
+    { name: "Pizza", node: null },
+    { name: "Stay home and cook dinner", node: null },
+  ],
+};
+
+const pizzaScene = {
+  title: "On my way to get a Pizza",
+  text: /*html*/ `
+        <p>FeelsGoodMan, it's that time of the week... time to get that salad kebab pizza baby!</p>
+        <p>I been thinking 'bout it, oh no, no, no... I been thinking about you, do you think about me still... kebab pizza yum...</p>
+        <p>THEY DONT HAVE KEBAB?!</p>`,
+  choices: [
+    { name: "Go home and cry", node: null },
+    { name: "Go home and cook dinner", node: null },
+  ],
+};
+
+const cookingScene = {
+  title: "Home and cooking dinner",
+  text: /*html*/ `
+        <p>Alright let's see what we're working with: *opens fridge as a tumbleweed passes by*</p>
+        <p>Im so tired I should have gotten the pizza</p>
+        <p>We're out of milk too, so no cereal... no cup noodles either. A dire situation</p>`,
+  choices: [
+    { name: "Go get a pizza", node: null },
+    { name: "Go grocery shopping", node: null },
+  ],
+};
+
+const cryingScene = {
+  title: "Crying at home",
+  text: /*html*/ `
+        <p>gg wp</p>`,
+  choices: [],
+};
+
+const shoppingScene = {
+  title: "Grocery Shopping",
+  text: /*html*/ `
+        <p>Alright lets just get a frozen pizza and some snacks for now.</p>
+        <p>It's not the kebab pizza but it'll do... I guess</p>
+        <p>What a day... I'm just gonna go home and eat now</p>
+        `,
+  choices: [{ name: "Go home", node: null }],
 };
 
 function start() {
   console.log("JavaScript kører...");
   registerButtonClicks();
-  currentScene = scene1;
+  currentScene = startScene;
   showScene(currentScene);
 }
 
@@ -104,3 +131,22 @@ function showScene(scene) {
 
   document.querySelector("main").insertAdjacentHTML("beforeend", html);
 }
+
+function setNodes(scenes, nodes) {
+  scenes.forEach((scene, index) => {
+    scene.choices.forEach((choice, choiceIndex) => {
+      choice.node = nodes[index][choiceIndex];
+    });
+  });
+}
+
+setNodes(
+  [startScene, lakeScene, homeScene, pizzaScene, cookingScene],
+  [
+    [lakeScene, homeScene], // nodes for startScene choices
+    [pizzaScene, cookingScene], // nodes for lakeScene choices
+    [pizzaScene, cookingScene], // nodes for homeScene choices
+    [cryingScene, cookingScene], // nodes for pizzaScene choices
+    [cryingScene, shoppingScene], // nodes for cookingScene choices
+  ]
+);
